74fb0ba8a470a96ecec2a2c565a8740f1ed8f1ff
Chris Tralie <chris.tralie@gmail.com>
2020-11-30 20:32:19 -0500

    More progress translating

--- DSP.cpp
+++ DSP.cpp
@@ -1,24 +1,74 @@
+/**
+ * Programmer: Chris Tralie
+ * Purpose: Vanilla C++ implementation of the Fast-Fourier Transform
+ * and Short-Time Fourier Transform
+ */
+
 #include <complex>
 #include <math.h>
 
 using namespace std;
 
+typedef complex<double> cdouble;
+
+/**
+ * Compute the closest power of 2 greater than or equal
+ * to some number
+ * @param a The number
+ * @return The closest power of 2 >= a
+ */
+int getClosestPowerOf2(int a) {
+	double lg = log((double)a) / log(2.0);
+	int power = (int)lg;
+	if ((double)((int)lg) < lg) {
+		power++;
+	}
+	return power;
+}
+
+/**
+ * Compute a version of x which is bit-reversed
+ * @param x A 32-bit int to reverse
+ * @param length Length of bits
+ * @return Bit-reversed version of x
+ */
+int bitReverse(int x, int length) {
+	int toReturn = 0;
+	int mirror = length / 2;
+	for (int mask = 1; mask <= length; mask <<= 1, mirror >>= 1) {
+		if ((mask & x) > 0)
+			toReturn |= mirror;
+	}
+	return toReturn;
+}
+
+
+/**
+ * Rearrange the terms in-place so that they're sorted by the least
+ * significant bit (this is the order in which the terms are accessed
+ * in the FFT)
+ * @param a An array of complex numbers
+ * @param N Number of elements in the array
+ */
+void rearrange(cdouble* a, int N) {
+	for (int i = 0; i < N; i++) {
+		int j = bitReverse(i, N);
+		if (j > i) { //Don't waste effort swapping two mirrored
+		//elements that have already been swapped
+			cdouble temp = a[j];
+			a[j] = a[i];
+			a[i] = temp;
+		}
+	}
+}
+
 class DSP {
 	private:
 		const int FORWARD = 0;
 		const int INVERSE = 1;
-		complex<double>*** W;//Cache the complex coefficients for 
-		complex<double>* hannwindow;
+		cdouble*** W;//Cache the complex coefficients for 
+		cdouble* hannwindow;
 		int fftsize;
-
-		int getClosestPowerOf2(int a) {
-			double lg = log((double)a) / log(2.0);
-			int power = (int)lg;
-			if ((double)((int)lg) < lg) {
-				power++;
-			}
-			return power;
-		}
 
 		/**
 		 * Initialize the complex coefficients for the FFT
@@ -27,27 +77,77 @@ class DSP {
 		void initCoeffs(int fftsize) {
 			int maxlevel = getClosestPowerOf2(fftsize) + 1;
 			W = new Complex[maxlevel + 1][2][];
-			W = new complex<double>**[maxlevel+1];
+			W = new cdouble**[maxlevel+1];
 			for (int level = 1; level <= maxlevel; level++) {
 				int FFTSize = 1 << level;
-				W[level] = new complex<double>*[2];
-				W[level][0] = new complex<double>[FFTSize >> 1];
-				W[level][1] = new complex<double>[FFTSize >> 1];
+				W[level] = new cdouble*[2];
+				W[level][0] = new cdouble[FFTSize >> 1];
+				W[level][1] = new cdouble[FFTSize >> 1];
 				for (int i = 0; i < FFTSize >> 1; i++) {
 					double iangle = (double)i * 2.0 * M_PI / (double)FFTSize;
 					double fangle = (-1.0) * iangle;
-					W[level][FORWARD][i] = complex<double>(cos(fangle), sin(fangle));
-					W[level][INVERSE][i] = complex<double>(cos(iangle), sin(iangle)); 
+					W[level][FORWARD][i] = cdouble(cos(fangle), sin(fangle));
+					W[level][INVERSE][i] = cdouble(cos(iangle), sin(iangle)); 
 				}
 			}
 		}
+
+		/**
+		 * Initialize the coefficients in Hann window
+		 * @param fftsize Lenght of fft
+		 */
+		void initWindow(int fftsize) {
+			int N = fftsize;
+			hannwindow = new double[N];
+			for (int n = 0; n < N; n++) {
+				double angle = 2.0*M_PI * n / (double)(N - 1);
+				//Do a Hamming hannwindow for now
+				hannwindow[n] = 0.54 - 0.46*Math.cos(angle);
+			}
+		}
+
+		/**
+		 * Perform an in-place Cooley-Tukey FFT
+		 * @param toReturn Array that holds FFT coefficients
+		 * @param N Length of array
+		 * @param inverse Whether this is a forward or inverse FFT
+		 */
+		cdouble* performfft(cdouble* toReturn, int N, int inverse) {
+			rearrange(toReturn, N);
+			//Do the trivial FFT size of 2 first
+			for (int i = 0; i < N; i += 2) {
+				Complex temp = toReturn[i];
+				toReturn[i] = temp.add(toReturn[i + 1]);
+				toReturn[i + 1] = temp.subtract(toReturn[i + 1]);
+			}
+			int Mindex = 2;//Index used to access the cached complex
+			//coefficients
+			for (int level = 2; level < N; level <<= 1) {
+				double angle = 0.0;
+				int FFTSize = level << 1;
+				for (int start = 0; start < N; start += FFTSize) {
+					//This is a little chunk of an FFT of size "FFTSize"
+					//to do in-place with the merging algorithm
+					//NOTE: "level" gives the length between mirrored terms
+					for (int i = 0; i < level; i++) {
+						Complex coeff = W[Mindex][inverse][i];
+						Complex first = toReturn[start + i];
+						Complex second = toReturn[start + i + level].mult(coeff);
+						toReturn[start + i] = first.add(second);
+						toReturn[start + i + level] = first.subtract(second);
+					}
+				}
+				Mindex++;
+			}
+			return toReturn;
+		}
 	
 	public:
-		complex<double> fftres;
+		cdouble fftres;
 		DSP(int fftsize) {
+			this->fftsize = fftsize;
 			this->initComplex(fftsize);
 			this->initWindow(fftsize);
-			this->fftsize = fftsize;
 		}
 		~DSP() {
 			int maxlevel = getClosestPowerOf2(fftsize) + 1;
@@ -60,90 +160,26 @@ class DSP {
 			}
 			delete[] W;
 			// Clean up window coefficients
+			delete[] hannwindow;
 		}
-	
-	private 
-	
-	private void initWindow(int fftsize) {
-		int N = fftsize;
-		hannwindow = new double[N];
-		for (int n = 0; n < N; n++) {
-			double angle = 2.0*M_PI * n / (double)(N - 1);
-			//Do a Hamming hannwindow for now
-			hannwindow[n] = 0.54 - 0.46*Math.cos(angle);
-		}
-	}
-	
-	private int bitReverse(int x, int length) {
-		int toReturn = 0;
-		int mirror = length / 2;
-		for (int mask = 1; mask <= length; mask <<= 1, mirror >>= 1) {
-			if ((mask & x) > 0)
-				toReturn |= mirror;
-		}
-		return toReturn;
-	}
-	
-	//Rearrange the terms so that they're sorted by the least
-	//significant bit (this is the order in which the terms are accessed
-	//in the FFT)
-	private void rearrange(Complex[] a) {
-		for (int i = 0; i < a.length; i++) {
-			int j = bitReverse(i, a.length);
-			if (j > i) { //Don't waste effort swapping two mirrored
-			//elements that have already been swapped
-				Complex temp = a[j];
-				a[j] = a[i];
-				a[i] = temp;
-			}
-		}
-	}
-	
-	//Implement the dft directly from the definition (used for speed comparison)
-	public Complex[] dft(Complex[] sig) {
-		Complex[] toReturn = new Complex[sig.length];
-		int N = toReturn.length;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				double angle = -2.0 * M_PI * (double)i * (double)j / (double)N;
-				Complex coeff = new Complex(Math.cos(angle), Math.sin(angle));
-				toReturn[i] = sig[i].mult(coeff);
-			}
-		}
-		return toReturn;
-	}
-	
-	private Complex[] performfft(Complex[] toReturn, int inverse) {
-		//Perform an in-place Cooley-Tukey FFT
-		rearrange(toReturn);
-		int N = toReturn.length;
-		//Do the trivial FFT size of 2 first
-		for (int i = 0; i < N; i += 2) {
-			Complex temp = toReturn[i];
-			toReturn[i] = temp.add(toReturn[i + 1]);
-			toReturn[i + 1] = temp.subtract(toReturn[i + 1]);
-		}
-		int Mindex = 2;//Index used to access the cached complex
-		//coefficients
-		for (int level = 2; level < N; level <<= 1) {
-			double angle = 0.0;
-			int FFTSize = level << 1;
-			for (int start = 0; start < N; start += FFTSize) {
-				//This is a little chunk of an FFT of size "FFTSize"
-				//to do in-place with the merging algorithm
-				//NOTE: "level" gives the length between mirrored terms
-				for (int i = 0; i < level; i++) {
-					Complex coeff = W[Mindex][inverse][i];
-					Complex first = toReturn[start + i];
-					Complex second = toReturn[start + i + level].mult(coeff);
-					toReturn[start + i] = first.add(second);
-					toReturn[start + i + level] = first.subtract(second);
+
+		/**
+		 * Implement the dft directly from the definition (used for speed comparison)
+		 * @param sig Complex signal on which to compute dft
+		 * @param N Length of signal
+		 * @return Complex DFT coefficients
+		 */
+		cdouble* dft(cdouble* sig, int N) {
+			cdouble* toReturn = new cdouble[N];
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					double angle = -2.0 * M_PI * (double)i * (double)j / (double)N;
+					Complex coeff = new cdouble(cos(angle), sin(angle));
+					toReturn[i] = sig[i].mult(coeff);
 				}
 			}
-			Mindex++;
+			return toReturn;
 		}
-		return toReturn;
-	}
 	
 	public Complex[] fft(Complex[] sig) {
 		Complex[] toReturn = new Complex[sig.length];
